84753e58480d860ec7bf2ef724598c7db00a7aff
Egor6052 <101550518+Egor6052@users.noreply.github.com>
2023-09-12 17:53:53 +0300

    0.1
    0.1

---
+++ App1/App1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace App1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Random randomNumber = new Random();
+            int firstNumber = randomNumber.Next(-100, 100), secondNumber = randomNumber.Next(-100, 100);
+            
+            Console.WriteLine("(" + firstNumber + " * " + secondNumber + ")^2 = " + ToSquare(firstNumber, secondNumber));
+        }
+
+        /// <summary>
+        /// Функция возведения в квадрат произведения двух целых чисел
+        /// </summary>
+        /// <param name="firstNumber">Первое целое число</param>
+        /// <param name="secondNumber">Второе целое число</param>
+        /// <returns>Возведенное в квадрат произведение двух целых чисел</returns>
+        static int ToSquare(int firstNumber, int secondNumber)
+        {
+            return (firstNumber * secondNumber) * (firstNumber * secondNumber);
+        }
+    }
+}
